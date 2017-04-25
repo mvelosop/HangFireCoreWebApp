8e038777d53e65dddc94f29a6d514766164afcd6
Miguel Veloso <mvelosop@gmail.com>
2017-04-25 17:09:00 +0100

    Include Core module

---
+++ src/HangFireCore.Core/HangfireJobAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HangFireCore.Core
+{
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    sealed class HangfireJobAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly int minutes;
+
+        // This is a positional argument
+        public HangfireJobAttribute(int minutes)
+        {
+            this.minutes = minutes;
+        }
+
+        public int Minutes
+        {
+            get { return minutes; }
+        }
+    }
+}
